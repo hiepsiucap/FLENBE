334cf6ac97b4f5003fae516d67d398045b585b9d
hiepsiucap <billhiepnguyen@gmail.com>
2025-09-28 22:30:14 +0700

    Update Create Bookstore

--- Controller/BookController.js
+++ Controller/BookController.js
@@ -5,28 +5,39 @@ const CustomAPIError = require("../errors");
 const Book = require("../Model/Book");
 const Session = require("../Model/Session");
 const CreateBook = async (req, res) => {
-  const { user } = req;
-  console.log(user);
-  console.log(req.file);
-  if (!req.body["name"] || !req.file) {
-    throw new CustomAPIError.BadRequestError("Vui lòng điền đầy đủ thông tin");
-  } else {
+  try {
+    const { user } = req;
+
+    // Get name from body, handle both "name" and "name " (with space)
+    const bookName = req.body["name"] || req.body["name "];
+
+    if (!bookName || !req.file) {
+      throw new CustomAPIError.BadRequestError(
+        "Vui lòng điền đầy đủ thông tin"
+      );
+    }
+
     const book = await Book.create({
       user: user.User_id,
       ava: req.file.path,
-      name: req.body["name"],
+      name: bookName,
     });
+
     const session = await Session.create({
       user: user.User_id,
       book: book._id,
       totalquestion: 0,
     });
+
     return res.status(StatusCodes.OK).json({ book });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: error.message,
+    });
   }
 };
 const GetBookByUser = async (req, res) => {
   const { user } = req;
-  console.log(user);
 
   const listbook = await Session.getBooksWithSessionAndCards(user.User_id);
   return res.status(StatusCodes.OK).json({ listbook });
